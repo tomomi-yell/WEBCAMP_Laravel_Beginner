40f62ba785b0987ebcdc7e767bf5f483a1bce747
TOMOMI <tomomi@example.com>
2024-10-18 02:25:28 +0000

    タスクの登録処理まで書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,19 +1,31 @@
-<?php
-
-declare(strict_types=1);
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class TaskController extends Controller
-{
     /**
-     * タスク一覧ページ を表示する
-     *
-     * @return \Illuminate\View\View
+     * タスクの新規登録
      */
-    public function list()
+    public function register(TaskRegisterPostRequest $request)
     {
-        return view('task.list');
+        // validate済みのデータの取得
+        $datum = $request->validated();
+        //
+        //$user = Auth::user();
+        //$id = Auth::id();
+        //var_dump($datum, $user, $id); exit;
+
+        // user_id の追加
+        $datum['user_id'] = Auth::id();
+
+        // テーブルへのINSERT
+        try {
+            $r = TaskModel::create($datum);
+        } catch(\Throwable $e) {
+            // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
+            echo $e->getMessage();
+            exit;
+        }
+
+        // タスク登録成功
+        $request->session()->flash('front.task_register_success', true);
+
+        // リダイレクト
+        return redirect('/task/list');
     }
 }
