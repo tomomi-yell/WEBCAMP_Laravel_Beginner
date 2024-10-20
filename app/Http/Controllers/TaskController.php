b8ba1f9dd888096f7a098ec1ef7253540a96df46
TOMOMI <tomomi@example.com>
2024-10-20 15:38:31 +0000

    ページネーションを書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,12 +17,17 @@ class TaskController extends Controller
      */
     public function list()
     {
+
+        // 1Page辺りの表示アイテム数を設定
+        $per_page = 2;
+
         // 一覧の取得
         $list = TaskModel::where('user_id', Auth::id())
-                        ->orderBy('priority', 'DESC')
-                        ->orderBy('period')
-                        ->orderBy('created_at')
-                        ->get();
+                         ->orderBy('priority', 'DESC')
+                         ->orderBy('period')
+                         ->orderBy('created_at')
+                         ->paginate($per_page);
+                        // ->get();
         /*
         $sql = TaskModel::where('user_id', Auth::id())
                         ->orderBy('priority', 'DESC')
